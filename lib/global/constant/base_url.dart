51c88f75c4382355b670d787ee1844a5aee6491a
GilangMF11 <gilangfahmi20@gmail.com>
2025-09-11 11:05:45 +0700

    check poin 11 sep 2025

--- lib/global/constant/base_url.dart
+++ lib/global/constant/base_url.dart
@@ -1,5 +1,5 @@
 class BaseUrl {
-  static String BASE_URL_DEV = 'http://172.16.246.75:3000/';
-  static String BASE_URL_STAGING = 'http://172.16.246.75:3000/';
-  static String BASE_URL = 'http://172.16.246.75:3000/';
+  static String BASE_URL_DEV = 'http://172.16.238.38:3000/';
+  static String BASE_URL_STAGING = 'http://172.16.238.38:3000/';
+  static String BASE_URL = 'https://healme.ump.ac.id/';
 }
